17c9516992f23a68d6507e335603f2f1f9825fe5
Jeri Atkins <jeri.atkins@student.swic.edu>
2019-10-04 01:40:18 -0500

    interacting with the form

--- scripts.js
+++ scripts.js
@@ -23,3 +23,17 @@ function divide(a, b) {
 function remainder(a, b) {
   return a % b;
 }
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const select = document.querySelector("select");
+
+//Use an anonymous callback function as the 2nd parameter
+form.addEventListener("submit", function(event) {
+  //Prevent the default browser behavior - DON"T SUBMIT
+  event.preventDefault();
+
+  // form inputs have a property called value
+  console.log(num1.value, num2.value, select.value);
+});
