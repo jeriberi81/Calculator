6d657b9ebf2404e7b44e29f50cdabc68ab4f6e8f
Jeri Atkins <jeri.atkins@student.swic.edu>
2019-10-12 12:52:15 -0500

    Move GLOBAL FUNCTIONS into an OBJECT LITERAL.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 // simple arithmetic calculator app.
 
-/**
- * 1. Move global functions into an object literal
- * 2. Use bracket notation to invoke the appropriate method
- */
-
 const calculator = {
   add(a, b) {
     return Number(a) + Number(b);
@@ -23,12 +18,11 @@ const calculator = {
   }
 };
 
-console.log(calculator.add(3, 3));
-
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 //Use an anonymous callback function as the 2nd parameter
 form.addEventListener("submit", function(event) {
@@ -36,5 +30,6 @@ form.addEventListener("submit", function(event) {
   event.preventDefault();
 
   // form inputs have a property called value
-  console.log(num1.value, num2.value, select.value);
+
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
